031db61908dca7658e508e9cd681de5e2678f373
enfermeria650vt-cell <enfermeria650vt@gmail.com>
2026-05-29 08:13:18 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = 'vt-v1';
+const ASSETS = [
+  '/medicamentos/',
+  '/medicamentos/index.html',
+  '/medicamentos/manifest.json',
+  '/medicamentos/icon-192.png',
+  '/medicamentos/icon-512.png',
+  '/medicamentos/icon.svg'
+];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
